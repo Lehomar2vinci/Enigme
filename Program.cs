d8e32e4e8ce3bfe3e39be378cd05fd6194c489f4
Nathan Chambrette ( Lehomar2vinci ) <67152033+Lehomar2vinci@users.noreply.github.com>
2023-01-27 13:00:10 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //preparation des variables
+            string trueWord = "chat";
+            string userWriteWord;
+
+            do
+            {
+                Console.WriteLine("Bonjour, voici une petite énigme :\n"
+                                + "\n"
+                                + "Je suis un animal de compagnie proche des humains \n"
+                                + "Nous n'aimons pas l'eau. à la fois agile et aimant le calme, j'aime dormir \n"
+                                +"\n"
+                                + "Qui suis-je ?\n"
+                                + "(Saisissez le nom de l'animal en minuscules)" );
+                userWriteWord = Console.ReadLine();
+                Console.WriteLine("Vous avez saisi : " + userWriteWord );
+            } while (userWriteWord != trueWord);
+
+            Console.WriteLine("Bravo c'etait bien le chat !! \n"
+                              + "\n"
+                              + "                   ~ Merci d'avoir joué et à la prochaine ! ~ ");
+
+        }
+    }
+}
